ac524198e3b00ffa30c8d056fbeb338dd3ab5817
CodeMode365 <codemode365@gmail.com>
2023-02-18 03:13:23 +0545

    strict mode removed

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,5 @@ import App from "./App";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
