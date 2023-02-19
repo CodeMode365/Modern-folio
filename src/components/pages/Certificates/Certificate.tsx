2006b052f14620ddc27bc50d11aa5bee11188b33
CodeMode365 <codemode365@gmail.com>
2023-02-19 15:23:06 +0545

    fallback

--- src/components/pages/Certificates/Certificate.tsx
+++ src/components/pages/Certificates/Certificate.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import LazyLoad from "react-lazyload";
 
 interface certificateProps {
   title: string;
@@ -11,19 +10,11 @@ const Certificate = ({ title, link, image }: certificateProps) => {
   return (
     <div className={`certificate`}>
       <a href={link} target="_blank">
-        <LazyLoad
-          once
-          height={300}
-          placeholder={
-            <img src="./img/load.png" style={{ filter: "grayscale(0)" }} />
-          }
-        >
           <img
             src={image}
             alt={title}
             style={{ width: "100%", height: "300px" }}
           />
-        </LazyLoad>
       </a>
       <div className="certificate-text">
         <h4>{title}</h4>
